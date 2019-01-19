d75035af8366a6a73334945e2fac4d1dc50e5e18
pYTR0N <lars.lmueller@gmail.com>
2019-01-19 15:22:58 +0100

    ADD: first html site

---
+++ src/main/java/game/files/web/GoController.java
@@ -0,0 +1,13 @@
+package game.files.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class GoController {
+    @GetMapping("/")
+    public String overview(Model model) {
+        return "overview";
+    }
+}
