3790216ff14b98de4d8683c74667c91c7ec5f05d
tnfru <lars.lmueller@gmail.com>
2019-09-15 01:40:33 +0200

    ADD: connecting groups by 2 half connections

--- src/main/java/game/files/model/LinkedGroup.java
+++ src/main/java/game/files/model/LinkedGroup.java
@@ -9,6 +9,8 @@ public class LinkedGroup {
     /*
      * Is the collection of groups which are connected indirectly but definitely
      * Ids contains all group ids which belong to the same connected group
+     * Friend := Stone has same Color as this Linked Group
+     *
      */
 
     private Stone[][] gameBoard;
@@ -37,93 +39,129 @@ public class LinkedGroup {
 
     }
 
+    private void addGroupsByFullConnectionPoints() {
+        List<Integer> idList = new ArrayList<>(this.ids);
+
+        for (int group = 0; group < idList.size(); group++) {
+            for (int row = 0; row < boardHeight; row++) {
+                for (int column = 0; column < boardHeight; column++) {
+                    updateFCPointsList(idList, group, row, column);
+                }
+            }
+        }
+        this.ids.addAll(idList);
+    }
+
     private void addGroupsByHalfConnectionPoints() {
-        List<Integer> idsList = new ArrayList<>(this.ids);
+        List<Integer> idList = new ArrayList<>(this.ids);
+        Map<Integer, Integer> groupHCPointsMap = new HashMap<>();
 
 
-    }
-
-    private int getGroupId(int row, int column) {
-        return gameBoard[row][column].getGroupId();
-    }
-
-    private Set<Integer> findHalfConnectionPoints(int row, int column, int groupId) {
-        if (isFieldEmpty(row, column) || !hasSameId(row, column, groupId)) {
-            return new HashSet<>();
+        for (int group = 0; group < idList.size(); group++) {
+            for (int row = 0; row < boardHeight; row++) {
+                for (int column = 0; column < boardHeight; column++) {
+                    updateHCPointsMap(idList, groupHCPointsMap, group, row, column);
+                    groupHCPointsMap.forEach((key, value) -> System.out.println(key + " " + value));
+                    updateIdList(idList, groupHCPointsMap);
+                }
+            }
         }
 
-        Set<Integer> groupIds = new HashSet<>();
-        Map<Integer, Integer> groupConnectionCount = new HashMap<>();
+        this.ids.addAll(idList);
+    }
 
+    private void updateFCPointsList(List<Integer> idList, int i, int row, int column) {
+        for (int id : findGroupIdsFCPoints(row, column, idList.get(i))) {
+            if (!idList.contains(id)) {
+                idList.add(id);
+            }
+        }
+    }
+
+    private void updateIdList(List<Integer> idList, Map<Integer, Integer> groupHCPointsMap) {
+        for (Integer id : groupHCPointsMap.keySet()) {
+            if (groupHCPointsMap.getOrDefault(id, 0) >= 2 &&
+                !idList.contains(id)) {
+                idList.add(id);
+            }
+        }
+    }
+
+    private void updateHCPointsMap(List<Integer> idList, Map<Integer, Integer> groupHCPointsMap,
+                                   int i, int row, int column) {
+        if (!isFieldEmpty(row, column) && isSameGroup(row, column, idList.get(i))) {
+            countDiagonalHCPoints(row, column, groupHCPointsMap);
+            countStraightHCPoints(row, column, groupHCPointsMap);
+        }
+    }
+
+    private void countStraightHCPoints(int row, int column,
+                                       Map<Integer, Integer> groupConnectionCount) {
+        if (isFieldFriend(row - 2, column) && isFieldEmpty(row - 1, column)) {
+            int fieldId = getGroupId(row - 2, column);
+            groupConnectionCount.put(fieldId, groupConnectionCount.getOrDefault(fieldId, 1));
+        }
+        if (isFieldFriend(row + 2, column) && isFieldEmpty(row + 1, column)) {
+            int fieldId = getGroupId(row + 2, column);
+            groupConnectionCount.put(fieldId, groupConnectionCount.getOrDefault(fieldId, 0) + 1);
+        }
+        if (isFieldFriend(row, column - 2) && isFieldEmpty(row, column - 1)) {
+            int fieldId = getGroupId(row, column - 2);
+            groupConnectionCount.put(fieldId, groupConnectionCount.getOrDefault(fieldId, 0) + 1);
+        }
+        if (isFieldFriend(row, column + 2) && isFieldEmpty(row, column + 1)) {
+            int fieldId = getGroupId(row, column + 2);
+            groupConnectionCount.put(fieldId, groupConnectionCount.getOrDefault(fieldId, 0) + 1);
+        }
+    }
+
+    private void countDiagonalHCPoints(int row, int column,
+                                       Map<Integer, Integer> groupConnectionCount) {
         if (isFieldFriend(row - 1, column - 1) &&
             (isFieldEmpty(row - 1, column) || isFieldEmpty(row, column - 1))) {
             int fieldId = getGroupId(row - 1, column - 1);
-            groupConnectionCount.put(fieldId, groupConnectionCount.getOrDefault(fieldId, 1));
+            groupConnectionCount.put(fieldId, groupConnectionCount.getOrDefault(fieldId, 0) + 1);
         }
 
         if (isFieldFriend(row - 1, column + 1) &&
             (isFieldEmpty(row - 1, column) || isFieldEmpty(row, column + 1))) {
             int fieldId = getGroupId(row - 1, column + 1);
-            groupConnectionCount.put(fieldId, groupConnectionCount.getOrDefault(fieldId, 1));
+            groupConnectionCount.put(fieldId, groupConnectionCount.getOrDefault(fieldId, 0) + 1);
         }
 
         if (isFieldFriend(row + 1, column - 1) &&
             (isFieldEmpty(row + 1, column) || isFieldEmpty(row, column - 1))) {
             int fieldId = getGroupId(row + 1, column - 1);
-            groupConnectionCount.put(fieldId, groupConnectionCount.getOrDefault(fieldId, 1));
+            groupConnectionCount.put(fieldId, groupConnectionCount.getOrDefault(fieldId, 0) + 1);
         }
 
         if (isFieldFriend(row + 1, column + 1) &&
             (isFieldEmpty(row + 1, column) || isFieldEmpty(row, column + 1))) {
             int fieldId = getGroupId(row + 1, column + 1);
-            groupConnectionCount.put(fieldId, groupConnectionCount.getOrDefault(fieldId, 1));
+            groupConnectionCount.put(fieldId, groupConnectionCount.getOrDefault(fieldId, 0) + 1);
         }
-
-
-        return groupIds;
     }
 
 
-    private void addGroupsByFullConnectionPoints() {
-        List<Integer> idsList = new ArrayList<>(this.ids);
-
-        for (int i = 0; i < idsList.size(); i++) {
-            for (int row = 0; row < boardHeight; row++) {
-                for (int column = 0; column < boardHeight; column++) {
-                    for (int id : findIdsFullConnectionPoints(row, column, idsList.get(i))) {
-                        if (!idsList.contains(id)) {
-                            idsList.add(id);
-                        }
-                    }
-                }
-            }
-        }
-        this.ids.addAll(idsList);
-    }
-
-    private Set<Integer> findIdsFullConnectionPoints(int row, int column, int groupId) {
-        if (gameBoard[row][column] == null || gameBoard[row][column].getGroupId() != groupId) {
+    private Set<Integer> findGroupIdsFCPoints(int row, int column, int groupId) {
+        if (isFieldEmpty(row, column) || !isSameGroup(row, column, groupId)) {
             return new HashSet<>();
         }
 
         Set<Integer> groupIds = new HashSet<>();
         if (isFieldFriend(row - 1, column - 1) &&
-            !hasSameId(row - 1, column - 1, groupId) &&
             isFieldEmpty(row - 1, column) && isFieldEmpty(row, column - 1)) {
             groupIds.add(gameBoard[row - 1][column - 1].getGroupId());
         }
         if (isFieldFriend(row - 1, column + 1) &&
-            !hasSameId(row - 1, column + 1, groupId) &&
             isFieldEmpty(row - 1, column) && isFieldEmpty(row, column + 1)) {
             groupIds.add(gameBoard[row - 1][column + 1].getGroupId());
         }
         if (isFieldFriend(row + 1, column - 1) &&
-            !hasSameId(row + 1, column - 1, groupId) &&
             isFieldEmpty(row, column - 1) && isFieldEmpty(row + 1, column)) {
             groupIds.add(gameBoard[row + 1][column - 1].getGroupId());
         }
         if (isFieldFriend(row + 1, column + 1) &&
-            !hasSameId(row + 1, column + 1, groupId) &&
             isFieldEmpty(row, column + 1) && isFieldEmpty(row + 1, column)) {
             groupIds.add(gameBoard[row + 1][column + 1].getGroupId());
         }
@@ -131,6 +169,10 @@ public class LinkedGroup {
         return groupIds;
     }
 
+
+    private int getGroupId(int row, int column) {
+        return gameBoard[row][column].getGroupId();
+    }
 
     private boolean isFieldFriend(int row, int column) {
         try {
@@ -140,7 +182,7 @@ public class LinkedGroup {
         }
     }
 
-    private boolean hasSameId(int row, int column, int groupId) {
+    private boolean isSameGroup(int row, int column, int groupId) {
         try {
             return gameBoard[row][column].getGroupId() == groupId;
         } catch (ArrayIndexOutOfBoundsException ignored) {
@@ -152,7 +194,7 @@ public class LinkedGroup {
         return gameBoard[row][column].isStoneWhite() == this.isWhite();
     }
 
-    private boolean isFieldEmpty(int i, int j) {
-        return gameBoard[i][j] == null;
+    private boolean isFieldEmpty(int row, int column) {
+        return gameBoard[row][column] == null;
     }
 }
